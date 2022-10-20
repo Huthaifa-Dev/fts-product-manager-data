9aa62030c6a507590965bfbe40c9364147992c64
Huthaifa Salman <H.j.Salman@students.ptuk.edu.ps>
2022-10-20 21:18:23 +0300

    added cart

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const categories = require('./data/categories.json')
 const products = require('./data/products.json')
-
+const cart = require('./data/cart.json')
 module.exports = {
     categories,
     products,
+    cart
 }
